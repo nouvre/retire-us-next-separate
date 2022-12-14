f3674a3b689df12aaba5435d8f130864cc7fed58
jcodewriter <jcodewriter@gmail.com>
2022-12-14 12:13:55 -0800

    fixed profilestep

--- src/store/plan/selector.ts
+++ src/store/plan/selector.ts
@@ -4,6 +4,6 @@ import { ApplicationState } from '@/store/index';
 export const getPlanState = createSelector(
   (state: ApplicationState) => state.auth.user,
   (user) => {
-    return { sPlan: user?.selected_plan ? true : false, cPlan: user?.current_plan ? true : false };
+    return { sPlan: user?.selected_plan ? true : (user?.current_plan ? true : false), cPlan: user?.current_plan ? true : false };
   }
 );
