14fcc7b6191fd8ea1115d3944f72d5b99512cfd8
jcodewriter <jcodewriter@gmail.com>
2022-12-21 05:43:53 -0800

    fixed 4518

--- next.config.js
+++ next.config.js
@@ -11,6 +11,16 @@ const nextConfig = {
       },
     ],
   },
+  experimental: {
+    headers() {
+      return [
+        {
+          source: "/.well-known/apple-app-site-association",
+          headers: [{ key: "content-type", value: "application/json" }]
+        }
+      ];
+    }
+  }
 }
 
 module.exports = nextConfig
